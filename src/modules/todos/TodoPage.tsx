8ab45e1b507c70414ddb5fcbe521a568872da9bc
Mohammad Khaki <mdkhaki10@gmail.com>
2022-06-22 16:52:43 +0430

    TodoPage UI completed

--- src/modules/todos/TodoPage.tsx
+++ src/modules/todos/TodoPage.tsx
@@ -1,16 +1,51 @@
 import { GetServerSideProps, NextPage } from 'next'
-import React from 'react'
+import Head from 'next/head'
+import React, { ChangeEvent } from 'react'
+import { useState } from 'react'
 import { BASE_URL } from 'shared/constants'
 import { Todo } from 'shared/models/Todo'
+import styles from './TodoPage.module.scss'
 
 const TodoPage: NextPage<{ todos: Todo[] }> = ({ todos }) => {
+  const [state, setState] = useState('')
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setState(e.currentTarget.value)
+  }
+
   return (
-    <>
-      <div>Todos: </div>
-      {todos.map((todo) => {
-        return <div>{todo.title}</div>
-      })}
-    </>
+    <div className={styles.container}>
+      <Head>
+        <title>Todo</title> 
+      </Head>
+      <main className={styles.main}>
+        <h1 className={styles.title}>Todo</h1>
+        <div className={styles.box}>
+          <div className={styles.bar} />
+          <input
+            placeholder="What needs to be done?"
+            className={styles.input}
+            value={state}
+            onChange={handleInputChange}
+          />
+          {todos.map((todo) => {
+            return (
+              <div className={styles.item}>
+                <p>{todo.title}</p><span>&#10006;</span>
+              </div>
+            )
+          })}
+          <div className={styles.optionsContainer}>
+            <div className={styles.leftItems}>3 items left</div>
+            <div>
+              <button className={styles.btn + ' ' + styles.active}>All</button>{' '}
+              <button className={styles.btn}>Active</button>
+            </div>
+            <button className={styles.btnClear}>Clear Completed</button>
+          </div>
+        </div>
+      </main>
+    </div>
   )
 }
 
