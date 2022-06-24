77429fa77a53782e379ac43baae40840eaf55989
Mohammad Khaki <mdkhaki10@gmail.com>
2022-06-24 23:34:27 +0430

    test removing todos

--- cypress/e2e/todo.cy.ts
+++ cypress/e2e/todo.cy.ts
@@ -1,21 +1,23 @@
 import { faker } from "@faker-js/faker";
 
 describe('todos CRUD', () => {
-  it('should allow you to add a new todo', () => {
-    const testTodo = {
-      title: faker.lorem.words(1),
-      completed: false
-    }
-
+  const testTodo = {
+    title: faker.lorem.words(1),
+    completed: false
+  }
+  before(() => {
     cy.visit('http://localhost:3000/todos')
+  })
+  it('should allow you to add a new todo', () => {
     cy.findByPlaceholderText('What needs to be done?',).type(testTodo.title)
     cy.findByRole('form',).submit()
 
-    // cy.findByText(testTodo.title)
+    cy.findByText(testTodo.title)
+  });
 
+  it('should allow you to remove todo', () => {
     cy.findByTestId(`delete-${testTodo.title}`).click()
-  }
-  )
+  });
 })
 
 export { }
